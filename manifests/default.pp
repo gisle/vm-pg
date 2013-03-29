bf6918b8370b267e7f1f0c3db5659601121d954d
Gisle Aas <gisle.aas@it.uib.no>
2013-03-29 17:59:36 +0100

    Control pg_hba.conf (copy of the file installed by postgres)

--- manifests/default.pp
+++ manifests/default.pp
@@ -6,3 +6,12 @@ service { "postgresql":
   ensure => running,
   require => Package["postgresql"],
 }
+
+file { "pg_hba.conf":
+  path => "/etc/postgresql/9.1/main/pg_hba.conf",
+  notify => Service["postgresql"],
+  ensure => present,
+  source => "/vagrant/pg_hba.conf",
+  owner => "postgres",
+  group => "postgres",
+}
