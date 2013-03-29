06184fcfc026ba077da3c3d30bcf4a5a300cd6a5
Gisle Aas <gisle.aas@it.uib.no>
2013-03-29 23:18:36 +0100

    Missing deps needed to boot from scratch

--- manifests/default.pp
+++ manifests/default.pp
@@ -1,5 +1,11 @@
+exec { 'apt-get update':
+  command => '/usr/bin/apt-get update',
+  onlyif => "/bin/bash -c 'exit $(( $(( $(date +%s) - $(stat -c %Y /var/lib/apt/lists/$( ls /var/lib/apt/lists/ -tr1|tail -1 )) )) <= 604800 ))'",
+}
+
 package { [ "postgresql" ]:
   ensure => present,
+  require => Exec['apt-get update'],
 }
 
 service { "postgresql":
@@ -9,6 +15,7 @@ service { "postgresql":
 
 file { "pg_hba.conf":
   path => "/etc/postgresql/9.1/main/pg_hba.conf",
+  require => Package["postgresql"],
   notify => Service["postgresql"],
   ensure => present,
   source => "/vagrant/pg_hba.conf",
